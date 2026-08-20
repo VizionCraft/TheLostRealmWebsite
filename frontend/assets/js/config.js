0e4bfe361f50d5a9fc55c5244453e6feb63d869d
VizionCraft <inventorguy8@gmail.com>
2026-08-20 15:54:27 -0500

    Update config.js

--- frontend/assets/js/config.js
+++ frontend/assets/js/config.js
@@ -2,7 +2,7 @@ window.TLR_CONFIG = {
   // Leave blank while testing the static site on Cloudflare Pages.
   // Later set this to your public backend, for example:
   // API_BASE_URL: "https://api.thelostrealm.org",
-  API_BASE_URL: "",
+  API_BASE_URL: "https://the-lost-realm-api.inventorguy8.workers.dev/",
 
   SERVER_IP: "play.thelostrealm.org",
   DISCORD_INVITE: "https://discord.gg/BAHjdnYkE5",
